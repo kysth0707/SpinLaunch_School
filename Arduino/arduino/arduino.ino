c91295c0d69056937977f8de53a4766c39c3e6c0
Kim TaeHyeong <kysth0707@naver.com>
2024-05-15 21:14:43 +0900

    AT 명령어 테스트 중

--- Arduino/arduino/arduino.ino
+++ Arduino/arduino/arduino.ino
@@ -1,9 +1,18 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <SoftwareSerial.h>
 
+SoftwareSerial BTSerial(4, 5);
+
+void setup() 
+{
+    Serial.begin(9600);
+    BTSerial.begin(9600);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() 
+{
+    if(BTSerial.available())
+        Serial.write(BTSerial.read());
 
+    if(Serial.available())
+        BTSerial.write(Serial.read());
 }
